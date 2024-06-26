2370b38de055e1a9130d58e4cee3847324feebce
Umesh604 <umesh604@gmail.com>
2024-06-26 15:24:27 +0530

    create admin panel sidebar component

---
+++ app/admin/addProduct/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default page
