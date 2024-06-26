2312ce701028b2584a496547a69d3f0039b39c4d
Umesh604 <umesh604@gmail.com>
2024-06-26 16:26:23 +0530

    add data in to database using frontend

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,25 +1,68 @@
 "use client"
 import { assets } from '@/Assets/assets'
+import axios from 'axios'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const page = () => {
 
     const [image,setImage] = useState(false)
+    const [data,setData] = useState({
+        title:"",
+        description:"",
+        category:"Startup",
+        author:"Judhistir Behera",
+        authorImg:"/author_img.png",
+    })
+
+    const onChangehandler = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+        console.log(data);
+    }
+
+    const onSubmitHandler = async (e) =>{
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('title',data.title);
+        formData.append('description',data.description);
+        formData.append('category',data.category);
+        formData.append('author',data.author);
+        formData.append('authorImg',data.authorImg);
+        formData.append('image',image)
+
+        const response = await axios.post('/api/blog',formData);
+        if(response.data.success){
+            toast.success(response.data.message)
+            setImage(false)
+            setData({
+                title:"",
+                description:"",
+                category:"Startup",
+                author:"Judhistir Behera",
+                authorImg:"/author_img.png",
+            })
+        }else{
+            toast.error("Error")
+        }
+    }
+
   return (
     <>
-      <form className='px-5 pt-5 sm:pt-12 sm:pl-16'>
+      <form onSubmit={onSubmitHandler} className='px-5 pt-5 sm:pt-12 sm:pl-16'>
         <p className='text-xl'>Upload Thumbnail</p>
         <label htmlFor="image">
             <Image src={!image?assets.upload_area:URL.createObjectURL(image)} height={70} width={140} alt='' className='mt-4'/>
         </label>
         <input onChange={(e)=>{setImage(e.target.files[0])}} type="file"  id="image" hidden required />
         <p className='text-xl mt-4'>Blog Title</p>
-        <input type="text" placeholder='Type here' required className='w-full sm:w-[500px] mt-4 px-4 py-3 border' />
+        <input name='title' value={data.title} onChange={onChangehandler} type="text" placeholder='Type here' required className='w-full sm:w-[500px] mt-4 px-4 py-3 border' />
         <p className='text-xl mt-4'>Blog Description</p>
-        <textarea type="text" placeholder='write content here' rows={6} required className='w-full sm:w-[500px] mt-4 px-4 py-3 border' />
+        <textarea name='description' value={data.description} onChange={onChangehandler} type="text" placeholder='write content here' rows={6} required className='w-full sm:w-[500px] mt-4 px-4 py-3 border' />
         <p className='text-xl mt-4'>Blog Category</p>
-        <select name="category" className='w-40 mt-4 px-4 py-3 border text-gray-500'>
+        <select name="category" onChange={onChangehandler} value={data.category} className='w-40 mt-4 px-4 py-3 border text-gray-500'>
             <option value="Startup">Startup</option>
             <option value="Technology">Technology</option>
             <option value="Lifestyle">Lifestyle</option>
