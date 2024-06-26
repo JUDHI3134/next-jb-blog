bddcceb57e8bdf3d4553980dccc217decf2e5c4f
Umesh604 <umesh604@gmail.com>
2024-06-26 10:59:51 +0530

    create blogList components

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <BlogItem/>
+      <BlogList/>
     </>
   );
 }
